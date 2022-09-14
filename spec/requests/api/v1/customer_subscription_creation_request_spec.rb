4fd19fea891feda5f4c3481fc78d90e5bc904159
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-09-14 15:30:31 -0600

    Happy path subscription sign up completed

--- spec/requests/api/v1/customer_subscription_creation_request_spec.rb
+++ spec/requests/api/v1/customer_subscription_creation_request_spec.rb
@@ -18,6 +18,13 @@ RSpec.describe 'Subscription Sign Up', type: :request do
       expect(response).to be_successful
       expect(new_subscription.customer_id).to eq(customer_sub_params[:customer_id])
       expect(new_subscription.subscription_id).to eq(customer_sub_params[:subscription_id])
+
+      new_sub = JSON.parse(response.body, symbolize_names: true)
+      # binding.pry
+      expect(new_sub[:data].keys).to include(:id, :type, :attributes)
+      expect(new_sub[:data][:attributes].keys).to include(:customer_id, :subscription_id)
+      expect(new_sub[:data][:attributes][:customer_id]).to eq(customer.id)
+      expect(new_sub[:data][:attributes][:subscription_id]).to eq(subscription.id)
     end
   end
 end
