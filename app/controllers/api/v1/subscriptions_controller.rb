fc74d72865d63f1c8aec9db09c4e998d81e9bade
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-09-14 15:25:19 -0600

    Functionality and serializer added for customer subscriptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def create
+    new_sub = CustomerSubscription.new(customer_sub_params)
     
+    # binding.pry
+    if new_sub.save
+      render json: CustomerSubscriptionSerializer.new(new_sub), status: :created
+    else
+      render status: 404
+    end
   end
+
+  private
+    def customer_sub_params
+      params.permit(:customer_id, :subscription_id)
+    end
 end
--- spec/requests/api/v1/customer_subscription_creation_request_spec.rb
+++ spec/requests/api/v1/customer_subscription_creation_request_spec.rb
@@ -5,16 +5,19 @@ RSpec.describe 'Subscription Sign Up', type: :request do
     it 'can create a new customer tea subscription' do 
       customer = create(:customer)
       subscription = create(:subscription)
+      # binding.pry
       customer_sub_params = ({
-                              customer: customer.id,
-                              subscription: subscription.id
+                              customer_id: customer.id,
+                              subscription_id: subscription.id
                             })
-      headers = {"CONTENT_TYPE" => "application/json"}
 
-      post "/api/v1/subscriptions"
-      new_subscription = Subscription.last
+      post "/api/v1/subscriptions", params: customer_sub_params
+      # binding.pry
+      new_subscription = CustomerSubscription.last
 
       expect(response).to be_successful
+      expect(new_subscription.customer_id).to eq(customer_sub_params[:customer_id])
+      expect(new_subscription.subscription_id).to eq(customer_sub_params[:subscription_id])
     end
   end
 end
