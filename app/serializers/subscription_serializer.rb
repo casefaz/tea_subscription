25e16ad7bf9ab8de60c91d3e1a1f6d9c35899e33
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-09-14 14:30:44 -0600

    Test for signing up for a subscription endpoint

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,4 +1,4 @@
 class SubscriptionSerializer
   include JSONAPI::Serializer
-  attributes :price, :active, :date_shipped, :frequency
+  attributes :price, :active, :date_shipped, :frequency, :name
 end
--- spec/requests/api/v1/customer_subscription_creation_request_spec.rb
+++ spec/requests/api/v1/customer_subscription_creation_request_spec.rb
@@ -3,7 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Subscription Sign Up', type: :request do 
   context 'happy path' do 
     it 'can create a new customer tea subscription' do 
-      
+      customer = create(:customer)
+
+      subscription_params = ({
+                              name: 'Monthly Tea Club',
+                              price: 30.00,
+                              date_shipped: "2022-09-14",
+                              frequency: 1
+                            })
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/subscriptions"
+      new_subscription = Subscription.last
+
+      expect(response).to be_successful
     end
   end
 end
