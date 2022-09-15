6850e8687c23d9bd503017f11a71f5cb51888305
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-09-14 18:59:35 -0600

    Update test for updating subscription status

--- spec/requests/api/v1/subscriptions/update_request_spec.rb
+++ spec/requests/api/v1/subscriptions/update_request_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Subscription Cancellation' do 
   context 'happy path' do
-    xit 'successfully deletes the customers subscription' do
+    it 'successfully deletes the customers subscription' do
       customers = create_list(:customer, 2)
       tiers = create_list(:tier, 3)
+      subscription = create(:subscription, customer_id: customers[0].id, tier_id: tiers[0].id, status: 0)
+      sub_params = { status: 1 }
 
-      cust_sub = create(:subscription, customer_id: customers[0].id, tier_id: tiers[0].id)
-      expect(CustomerSubscription.count, status: 1).to eq(1)
-
-      patch "/api/v1/customers/#{customers[0].id}/subscriptions/#{subscriptions[0].id}"
+      patch "/api/v1/customers/#{customers[0].id}/subscriptions/#{tiers[0].id}", params: sub_params
 
       expect(response).to be_successful
 
